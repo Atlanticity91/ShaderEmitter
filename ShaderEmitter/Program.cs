f463055ba638ae6c4754071ff0e5537890d43313
Alves Quentin <atlanticity91@gmail.com>
2021-12-13 21:45:56 +0100

    Add project files.

---
+++ ShaderEmitter/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ShaderEmitter {
+    internal class Program {
+        static void Main( string[] args ) {
+            Console.WriteLine( "Hello World!" );
+        }
+    }
+}
